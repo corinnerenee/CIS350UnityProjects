630f9916192af4bba8d63aceb5d5b907e31bdf7e
Corinne Bond <corinne32300@gmail.com>
2022-09-05 17:25:44 -0500

    challenge1

---
+++ Challenge1/Assets/Challenge 1/Scripts/LoseOnOutOfBounds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseOnOutOfBounds : MonoBehaviour
+{
+
+    public GameObject plane;
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(plane.transform.position.y > 80 || plane.transform.position.y < -51)
+        {
+            ScoreManager.gameOver = true;
+        }
+        
+    }
+}
---
+++ Challenge1/Assets/Challenge 1/Scripts/ScoreManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+
+    public Text textbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+        if ( score >= 5)
+        {
+            won = true;
+        }
+
+        if (gameOver)
+        {
+            if (won)
+            {
+                textbox.text = "You Win!\nPress Z to Play Again!";
+            }
+            else
+            {
+                textbox.text = "You Lost!\nPress Z to Play Again!";
+            }
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+    }
+}
