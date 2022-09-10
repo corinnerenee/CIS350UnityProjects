5c9cd33b74c04a5fb3ad5977bb5bcdd7de3f787b
Corinne Bond <corinne32300@gmail.com>
2022-09-10 16:32:36 -0500

    inProgress
    moveforward, playercontroller, and shootprefab scripts have been added

---
+++ Prototype2/Prototype2Runthrough/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿/*
+ Corinne Bond
+ Prototype2
+ This script controls player movements
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 14;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal"); //horizontal input from left/right or a/d
+
+        //transform horizontally w/ input
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //keeps player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+    }
+}
