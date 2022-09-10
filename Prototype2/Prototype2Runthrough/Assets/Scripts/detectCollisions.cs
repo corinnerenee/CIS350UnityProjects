8c85cc9d3a3ea05a3c4d89a0b6e4e74458344da1
Corinne Bond <corinne32300@gmail.com>
2022-09-10 16:51:33 -0500

    destroyOutOfBounds

---
+++ Prototype2/Prototype2Runthrough/Assets/Scripts/detectCollisions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//attach to food projectile prefab
+
+public class detectCollisions : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject); //destroy animal 
+        Destroy(gameObject); //destroy food
+    }
+}
