747f753719d91e8c716500a9ea7db38b6f7753bc
Corinne Bond <corinne32300@gmail.com>
2022-10-14 15:27:41 -0500

    scoreImpemented

---
+++ PennyPixel_2DTilemapProject/Assets/CollectableGem/Scripts/GemBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemBehaviour : MonoBehaviour
+{
+	[Header("References")]
+	public GameObject gemVisuals;
+	public GameObject collectedParticleSystem;
+	public CircleCollider2D gemCollider2D;
+    public ScoreManager scoreScript;
+
+	private float durationOfCollectedParticleSystem;
+
+
+	void Start()
+	{
+		durationOfCollectedParticleSystem = collectedParticleSystem.GetComponent<ParticleSystem>().main.duration;
+        scoreScript = GameObject.FindGameObjectWithTag("ScoreManager").GetComponent<ScoreManager>();
+	}
+
+	void OnTriggerEnter2D(Collider2D theCollider)
+	{
+		if (theCollider.CompareTag ("Player")) {
+			GemCollected ();
+		}
+	}
+
+	void GemCollected()
+	{
+		gemCollider2D.enabled = false;
+		gemVisuals.SetActive (false);
+		collectedParticleSystem.SetActive (true);
+		Invoke ("DeactivateGemGameObject", durationOfCollectedParticleSystem);
+        scoreScript.AddScore();
+
+	}
+
+	void DeactivateGemGameObject()
+	{
+		gameObject.SetActive (false);
+	}
+}
---
+++ PennyPixel_2DTilemapProject/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class ScoreManager : MonoBehaviour
+{
+    //private GemBehaviour gemScript;
+    public Text scoreText;
+    public Text eofgText; //end of game text for win or lose
+    public int score;
+    public bool won;
+    public bool gameOver;
+    public float timeValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         // = GameObject.FindGameObjectWithTag("GemBehaviour").GetComponent<GemBehaviour>();
+        scoreText.text = "Score: 0";
+        won = false;
+        gameOver = false;
+        timeValue = 120;
+    }
+
+    private void Update()
+    {
+        float min = Mathf.FloorToInt(timeValue / 60);
+        float sec = Mathf.FloorToInt(timeValue % 60);
+
+        if (timeValue > 0)
+        {
+            timeValue -= Time.deltaTime; 
+        }
+
+        if (timeValue >= 0 && score >= 10)
+        {
+            gameOver = true;
+            won = true;
+            eofgText.text = "You Won! Press A to Play Again!";
+        }
+
+        if( timeValue <= 0 && score < 10)
+        {
+            gameOver = true;
+            won = false;
+            eofgText.text = "You've ran out of time! Press A to Play Again";
+        }
+           
+        
+    }
+
+    public void AddScore()
+    {
+        //to increment
+        score++;
+
+        scoreText.text = "Score: " + score;
+    }
+}
