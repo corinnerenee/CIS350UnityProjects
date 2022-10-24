a2e08764807fe2e7c4e1faccc5a57821be7910ef
Corinne Bond <corinne32300@gmail.com>
2022-10-23 23:35:49 -0500

    finishedPrototype

--- 3DPrototype/Assets/MyFirstPersonPlayer/Scripts/PlayerMovement.cs
+++ 3DPrototype/Assets/MyFirstPersonPlayer/Scripts/PlayerMovement.cs
@@ -6,41 +6,52 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+    //gravity variables
     public Vector3 velocity;
     public float gravity = -9.81f;
     public float gravityMultipler = 2f;
+    //groundCheck variables
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
     public bool isGrounded;
     public float jumpHeight = 3f;
 
-    private void Awake()
+    void Awake()
     {
-        controller = GetComponent<CharacterController>();
-        gravity += gravityMultipler;
+        gravity *= gravityMultipler;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //check if  player is on the ground
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-    if(isGrounded && velocity.y < 0)
-    {
-                velocity.y = -2f; //reset gravity velocity to a low number
-    }
+        if(isGrounded && velocity.y < 0) 
+        {
+            velocity.y = -2f; //reset gravity velocity to a low number
+        }
 
+        //get input
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        //jump code
         if(Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
-        //after jumping add gravity to velocity
+        //add gravity to velocity
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+
+
+        
+        
     }
 }
--- 3DPrototype/Assets/MyFirstPersonPlayer/Scripts/ShootWRaycasts.cs
+++ 3DPrototype/Assets/MyFirstPersonPlayer/Scripts/ShootWRaycasts.cs
@@ -6,7 +6,10 @@ public class ShootWRaycasts : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100;
+    public float hitForce = 10f;
+
     public Camera cam;
+    public ParticleSystem muzzleFlash;
 
     void Update()
     {
@@ -15,11 +18,30 @@ public class ShootWRaycasts : MonoBehaviour
 
     void Shoot()
     {
+        //play at particle effect @ beginning of method
+        muzzleFlash.Play();
+
         RaycastHit hitInfo;
 
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
         {
             Debug.Log(hitInfo.transform.gameObject.name);
+            
+            //get target script from hit objct
+            TargetHealth target =
+                hitInfo.transform.gameObject.GetComponent<TargetHealth>();
+
+            //if target script ws found
+            if( target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            //if shot hits rigidbody, apply force
+            if(hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+            }
         }
     }
 }
