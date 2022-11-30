a15a95a225faaa917ae57668eb13500757257083
Corinne Bond <corinne32300@gmail.com>
2022-11-29 23:10:20 -0600

    showRestartButton
    Slide 62/83

---
+++ prototype5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+
+    public bool isGameActive;
+
+    private float spawnRate;
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);}} //end of SpawnTarget
+}
---
+++ prototype5/Assets/Scripts/Target.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
+    private Rigidbody targetRB;
+    private GameManager gameManager;
+    private float minSpeed = 12; private float maxSpeed = 16;
+    private float maxTorque = 10; private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRB = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        targetRB.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        targetRB.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+           Destroy(gameObject);
+           Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+           gameManager.UpdateScore(pointValue);
+        }
+     
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    Vector3 RandomForce() {  return Vector3.up * Random.Range(minSpeed, maxSpeed); }
+    Vector3 RandomSpawnPos() { return new Vector3(Random.Range(-xRange, xRange), ySpawnPos); }
+    float RandomTorque() { return Random.Range(-maxTorque, maxTorque); }
+
+}
